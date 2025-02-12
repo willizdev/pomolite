5d0fef8c524dc633b8fef8c765c7b1a60996103b
wz73 <williamzheng73@gmail.com>
2025-02-12 07:32:05 -0300

    add svelte base app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../app.scss";
+</script>
+
+<svelte:head>
+    <title>Pomolite</title>
+</svelte:head>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<div>
+    <h1>Pomolite</h1>
+    <p>A fast, simple, and distraction-free Pomodoro timer to boost your focus and productivity</p>
+</div>
+
+<style lang="scss">
+    div {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        h1 {
+            margin-bottom: 20px;
+            font-size: 5em;
+        }
+
+        p {
+            font-size: 2em;
+        }
+    }
+</style>
