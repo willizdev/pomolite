38d764a9b365f96a2b914029471d10598504165e
wz73 <williamzheng73@gmail.com>
2025-02-15 07:02:18 -0300

    add switch and number input components

---
+++ src/lib/components/input.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+    import { ChevronUp, ChevronDown } from "lucide-svelte";
+
+    let { value, onchange = () => {}, min = 0, max = undefined } = $props();
+</script>
+
+<div class="input">
+    <div class="value">
+        <span>{value}</span>
+    </div>
+    <div class="control">
+        <button
+            onclick={() => {
+                if (max === undefined || value < max) {
+                    onchange(++value);
+                }
+            }}
+        >
+            <ChevronUp size={16} strokeWidth={4} />
+        </button>
+        <button
+            onclick={() => {
+                if (min === undefined || value > min) {
+                    onchange(--value);
+                }
+            }}><ChevronDown size={16} strokeWidth={4} /></button
+        >
+    </div>
+</div>
+
+<style lang="scss">
+    .input {
+        display: flex;
+    }
+
+    .value {
+        width: 4em;
+        height: 2.8em;
+        border: 1px #c6c6c6 solid;
+        border-top-left-radius: 0.8em;
+        border-bottom-left-radius: 0.8em;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .control {
+        height: 2.8em;
+        display: flex;
+        flex-direction: column;
+
+        button {
+            height: 50%;
+            width: 2em;
+            color: #a6a6a6;
+            border: none;
+            background: transparent;
+            border: 1px #c6c6c6 solid;
+            cursor: pointer;
+            padding-top: 0.2em;
+        }
+
+        button:nth-child(1) {
+            border-left: none;
+            border-bottom: none;
+            border-top-right-radius: 0.8em;
+        }
+
+        button:nth-child(2) {
+            border-left: none;
+            border-bottom-right-radius: 0.8em;
+        }
+    }
+</style>
---
+++ src/lib/components/switch.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    let { active = false, activeClass = "", onclick = () => {} } = $props();
+    const toggleSwitch = function () {
+        active = !active;
+        onclick();
+    };
+</script>
+
+<div class="switch">
+    <button
+        onclick={toggleSwitch}
+        aria-label="switch"
+        class={active ? `active ${activeClass}` : ""}
+    >
+        <span></span>
+    </button>
+</div>
+
+<style lang="scss">
+    .switch button {
+        display: block;
+        height: 2em;
+        width: 3.5em;
+        position: relative;
+        background: #c6c6c6;
+        border-radius: 1em;
+        cursor: pointer;
+        border: none;
+        transition: 0.3s;
+
+        &.active span {
+            left: 1.75em;
+        }
+
+        span {
+            height: 1.5em;
+            width: 1.5em;
+            top: 0.25em;
+            left: 0.3em;
+            position: absolute;
+            border-radius: 100%;
+            background: white;
+            transition: 0.3s;
+        }
+    }
+</style>
